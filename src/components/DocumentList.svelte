8a941b03bd7f2db598ce836a97446c55968c0d22
Thor Stenbæk <tst@dips.no>
2021-04-11 22:10:20 +0200

    Added DocumentList html and style

---
+++ src/components/DocumentList.svelte
@@ -0,0 +1,34 @@
+<h1>Document List</h1>
+
+<table>
+    <thead>
+        <th>Date</th><th>Title</th><th>Author</th>
+    </thead>
+    <tbody>        
+        <tr>
+            <td>document.eventDate</td>
+            <td>document.title</td>
+            <td>document.author</td>
+        </tr>        
+    </tbody>
+</table>
+
+<style>
+    table,th,td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+
+    th, td {
+        padding: 2px 5px 2px 5px;
+    }
+
+    tr:nth-child(even) {background-color: #eeeeee;}
+    tr:nth-child(odd) {background-color: #ffffff;}
+
+    table {                
+        background: #eeeeee;
+        width:50%;
+        text-align: left;
+    }
+</style>
