eb6b901f747a3ef01339df9af1c39c6b195317dc
Thor Stenbæk <tst@dips.no>
2021-04-11 22:47:43 +0200

    Format dates and names

--- src/components/DocumentList.svelte
+++ src/components/DocumentList.svelte
@@ -2,19 +2,43 @@
 
 <script>
     import documents from "../data/documents.json";       
+
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    function formatDate(dateString) {
+        var d = new Date(dateString);
+        var now = new Date();
+        if (now.getFullYear() == d.getFullYear()) {
+            return `${String(d.getDate()).padStart(2, '0')}. ${monthNames[d.getMonth()]} ${String(d.getHours()).padStart(2, '0')}.${String(d.getMinutes()).padStart(2, '0')}`;
+        }
+        
+        return `${String(d.getDate()).padStart(2, '0')}.${String(d.getMonth()).padStart(2, '0')}.${d.getFullYear()}`;
+    }
+
+    function formatName(s) {
+        if (typeof s !== 'string') return ''
+        var lowerCase = s.toLowerCase();
+        return lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);        
+    }
+
 </script>
 
 <table>
     <thead>
-        <th>Date</th><th>Title</th><th>Last Name</th><th>First Name</th>
+        <th>Date</th>
+        <th>Title</th>
+        <th>Last Name</th>
+        <th>First Name</th>
     </thead>
     <tbody> 
         {#each documents as document}       
         <tr>            
-            <td>{document.eventTime}</td>
+            <td>{formatDate(document.eventTime)}</td>
             <td>{document.title}</td>
-            <td>{document.author.lastName}</td>        
-            <td>{document.author.firstName}</td>        
+            <td>{formatName(document.author.lastName)}</td>        
+            <td>{formatName(document.author.firstName)}</td>        
         </tr>        
         {/each}
     </tbody>
